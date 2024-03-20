23d6295aa7a1846f230d2f54df3fd1849441895f
einfachFriedrich <registrierrp@gmail.com>
2024-03-20 10:19:38 +0100

    1 Question Only works

--- Edutainment/ContentView.swift
+++ Edutainment/ContentView.swift
@@ -6,18 +6,63 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
+    @State private var Multiplicator = 2
+    @State private var ÜbungsfragenAnzahl = 1
+    @State private var guesses = Array(repeating: 0, count: 20)
+    @State private var guess = 0
+    @State private var MultiplicatorArray = [1, 4 , 3, 2 , 5, 7 , 6, 12, 10, 8, 9, 11].shuffled()
+    @State private var showAnswers = Array(repeating: false, count: 20)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            Form{
+                Section("Welchen Multiplikator möchtest du üben?"){
+                    Stepper("\(Multiplicator)", value: $Multiplicator)
+                }
+                Section("Wie viele Übungsfragen möchtest du?") {
+                    Picker("Test", selection: $ÜbungsfragenAnzahl) {
+                        ForEach(0..<21) {
+                            Text("\($0)")
+                        }
+                    }
+                }
+                Section("Übungsfragen"){
+                    ForEach(0..<ÜbungsfragenAnzahl, id: \.self){ number in
+                        let randomQuestionMultiplikator = MultiplicatorArray[number]
+                        HStack{
+                            VStack(alignment: .leading){
+                                Text("Was ist \(Multiplicator) * \(randomQuestionMultiplikator)?")
+                                TextField("Antwort", value: $guess, formatter: NumberFormatter())
+                                
+                                if showAnswers[number] == true{
+                                    if checkAnswer(RandomMultiplicator: randomQuestionMultiplikator, userAnswer: guess){
+                                        Text("Richtig ✅")
+                                    } else{
+                                        Text("Falsch ❌")
+                                    }
+                                }
+                            }
+                            Button("Überprüfen") {
+                                showAnswers[number] = true
+                            }
+                        }
+                    }
+                    
+                }
+            }
+            
+            .navigationTitle("Das kleine 1x1")
         }
-        .padding()
+    }
+    func checkAnswer(RandomMultiplicator : Int, userAnswer : Int) -> Bool{
+        if userAnswer == RandomMultiplicator * Multiplicator{
+            return true
+        }
+        return false
     }
 }
+
 
 #Preview {
     ContentView()
